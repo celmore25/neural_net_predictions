dfd8cb56c6cd901e684ec0f5968d108a5544f492
Grace Kopp <gkopp@crcfe01.crc.nd.edu>
2019-04-17 18:44:18 -0400

    add error calculation

--- implementation/main.py
+++ implementation/main.py
@@ -3,7 +3,7 @@ import rnn
 if __name__ == '__main__':
 
 	time_rnn = rnn.RNN()
-	time_rnn.load_data('../prices/prices.csv',.67, False);
-	time_rnn.split_timesteps(24, 10);
-	time_rnn.predict()
+	time_rnn.load_data('../prices/prices.csv',1, False);
+	#time_rnn.split_timesteps(24, 48);
+	#time_rnn.predict()
  
