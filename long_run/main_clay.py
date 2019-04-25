2a84c6ced3294a273d49be17a2bd3ae4fe0b31b3
Clay Elmore <celmore25@gmail.com>
2019-04-25 12:49:54 -0400

    try again

--- long_run/main_clay.py
+++ long_run/main_clay.py
@@ -4,10 +4,11 @@ import pandas as pd
 if __name__ == '__main__':
 
 	time_rnn = rnn.RNN()
-	dataframe = pd.read_csv('../prices/prices.csv',header = None).values
-	for i in range(7):
+	dataframe = pd.read_csv('../prices/all_prices.csv',header = None).values
+	for i in range(340):
 		print('=========================================================')
 		print('Testing Day',i)
 		print('=========================================================')
-		roll_data = dataframe.T[i*24:168 + i*24].T
-		time_rnn.load_data(roll_data,num_locs = 50)
+		train_num = 336
+		roll_data = dataframe.T[i*24:336 + i*24].T
+		time_rnn.load_data(roll_data,num_locs = 1)
